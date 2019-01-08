b687d0aafe8d6019ca3e5dbf00f3b20ee8bc6adb
ankit ranjan <ankit.ranjan@accolitelabs.com>
2019-01-08 23:37:57 +0530

    working customer model

--- src/main/java/com/prudential/datalake/reconciliation/model/transformed/Customer.java
+++ src/main/java/com/prudential/datalake/reconciliation/model/transformed/Customer.java
@@ -9,6 +9,14 @@ import org.springframework.data.couchbase.core.mapping.id.GenerationStrategy;
 import com.couchbase.client.java.repository.annotation.Id;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.prudential.datalake.reconciliation.model.CHDRPF;
+import com.prudential.datalake.reconciliation.model.REGPPF;
+import com.prudential.datalake.reconciliation.model.UWQSPF;
+import com.prudential.datalake.reconciliation.model.sub.Address;
+import com.prudential.datalake.reconciliation.model.sub.Billing;
+import com.prudential.datalake.reconciliation.model.sub.Contact;
+import com.prudential.datalake.reconciliation.model.sub.CustomerLifeStyle;
+import com.prudential.datalake.reconciliation.model.sub.Occupation;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,5 +36,29 @@ public class Customer implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationStrategy.UNIQUE)
 	private String id;
-	private String name;
+//	private Billing billing;
+//	private CustomerLifeStyle lifestyle;
+//	private Address addressDetails;
+	private String country;
+	private String surName;
+	private String sex;
+//	private Occupation occupation;
+	private String nationality;
+	private String client;
+//	private Contact contactDetails;
+	private String salution;
+	private String maritalStatus;
+	//TODO:  gives error
+//	private BankAccount[] bankAccounts;
+	
+	private String firstName;
+	private String middleName;
+	private String dob;
+	private String spouse;
+	private String children;
+//	private CHDRPF[] policies;
+//	private REGPPF[] claims;
+	private String agent;
+	private String payments;
+//	private UWQSPF[] medicalHistory;
 }
